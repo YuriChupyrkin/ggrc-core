ecdae4afcf85579e1b1ab5c7fa8753bf39e1c885
YuriChupyrkin <yu4.1990@gmail.com>
2017-08-16 16:02:37 +0300

    Add issue-unmap component

---
+++ src/ggrc/assets/javascripts/components/issue/issue-unmap-item.js
@@ -0,0 +1,43 @@
+/*!
+    Copyright (C) 2017 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+(function (can, GGRC) {
+  'use strict';
+
+  GGRC.Components('issueUnmapItem', {
+    tag: 'issue-unmap-item',
+    template: can.view(
+      GGRC.mustache_path +
+      '/components/issue/issue-unmap-item.mustache'
+    ),
+    viewModel: {
+      issueInstance: {},
+      target: {},
+      showRelatedObjects: false,
+      canUnmap: function () {
+        return GGRC.Utils.allowed_to_map(this.attr('issueInstance'),
+          this.attr('target'), {}, true);
+      },
+      unmap: function () {
+        if (this.attr('target.type') === 'Audit' &&
+          !this.attr('issueInstance.allow_unmap_from_audit')) {
+          this.attr('showRelatedAssessments', true);
+          return;
+        }
+
+        this.dispatch('unmapIssue');
+      },
+      modalClossed: function () {
+        this.attr('showRelatedAssessments', false);
+      }
+    },
+    events: {
+      click: function (el, ev) {
+        this.viewModel.unmap();
+        ev.preventDefault();
+      }
+    }
+  });
+})(window.can, window.GGRC);
