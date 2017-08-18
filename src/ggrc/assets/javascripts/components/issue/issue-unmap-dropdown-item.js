94671b30ef8e04684bb3f9a1809df89c7ddd5af9
YuriChupyrkin <yu4.1990@gmail.com>
2017-08-18 13:01:32 +0300

    Add issue-unmap related objects modal

---
+++ src/ggrc/assets/javascripts/components/issue/issue-unmap-dropdown-item.js
@@ -0,0 +1,30 @@
+/*!
+    Copyright (C) 2017 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+(function (can, GGRC) {
+  'use strict';
+
+
+  GGRC.Components('issueUnmapDropdownItem', {
+    tag: 'issue-unmap-dropdown-item',
+    template: can.view(
+      GGRC.mustache_path +
+      '/components/issue/issue-unmap-dropdown-item.mustache'
+    ),
+    viewModel: {
+      define: {
+        issueUnmap: {
+          get: function () {
+            return this.attr('page_instance.type') === 'Issue' ||
+              this.attr('instance.type') === 'Issue';
+          }
+        }
+      },
+      instance: {},
+      page_instance: {},
+      options: {}
+    }
+  });
+})(window.can, window.GGRC);
