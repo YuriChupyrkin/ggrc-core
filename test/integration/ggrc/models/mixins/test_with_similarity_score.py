d16e96912f4a5df7c198632c4e52f89b65923de3
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2017-09-05 11:56:22 +0300

    [WIP] Fix tests

--- test/integration/ggrc/models/mixins/test_with_similarity_score.py
+++ test/integration/ggrc/models/mixins/test_with_similarity_score.py
@@ -582,7 +582,7 @@ class TestWithSimilarityScore(TestCase):
         factories.RelationshipFactory(source=snapshot, destination=assessment)
 
       # Create one issue in the last audit linked to snapshot/assessment
-      issue = factories.IssueFactory(audit=audit)
+      issue = factories.IssueFactory()
       factories.RelationshipFactory(source=audit, destination=issue)
       factories.RelationshipFactory(source=snapshot, destination=issue)
       if asmnt_related:
