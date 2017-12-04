def942114e66e1331966fadb104df4fb90603e5b
pod2metra <pod2metra@gmail.com>
2017-12-04 20:55:13 +0300

    Add comment about create/apply/decline proposal

--- src/ggrc/models/hooks/proposal.py
+++ src/ggrc/models/hooks/proposal.py
@@ -7,21 +7,62 @@ from sqlalchemy import inspect
 
 from ggrc.services import signals
 from ggrc.models import all_models
+from ggrc.models import comment
+from ggrc import login
 
 
-def upsert_all(
+def is_status_changed_to(required_status, obj):
+  return (inspect(obj).attrs.status.history.has_changes() and
+          obj.status == required_status)
+
+
+def add_comment_to(obj, txt):
+  if not isinstance(obj, comment.Commentable):
+    return
+  created_comment = all_models.Comment(
+      description=txt,
+      modified_by_id=login.get_current_user_id())
+  all_models.Relationship(
+      source=obj,
+      destination=created_comment)
+
+
+def build_text_comment(txt, proposal_link):
+  return "{} \n\n link:{}".format(txt or "", proposal_link)
+
+
+def apply_proposal(
     sender, obj=None, src=None, service=None,
     event=None, initial_state=None):  # noqa
-  if not inspect(obj).attrs.status.history.has_changes():
-    return
-  if obj.status != obj.STATES.APPLIED:
+  if not is_status_changed_to(obj.STATES.APPLIED, obj):
     return
   for field, value in obj.content["fields"].iteritems():
     if hasattr(obj.instance, field):
       setattr(obj.instance, field, value)
+  add_comment_to(obj.instance, obj.apply_reason or "")
+
+
+def decline_proposal(
+    sender, obj=None, src=None, service=None,
+    event=None, initial_state=None):  # noqa
+  if not is_status_changed_to(obj.STATES.DECLINED, obj):
+    return
+  add_comment_to(obj.instance, obj.decline_reason or "")
+
+
+def make_proposal(
+    sender, obj=None, src=None, service=None,
+    event=None, initial_state=None):  # noqa
+  add_comment_to(obj.instance, obj.agenda or "")
 
 
 def init_hook():
-  signals.Restful.model_put.connect(upsert_all,
+  signals.Restful.model_posted.connect(make_proposal,
+                                       all_models.Proposal,
+                                       weak=False)
+  signals.Restful.model_put.connect(apply_proposal,
+                                    all_models.Proposal,
+                                    weak=False)
+  signals.Restful.model_put.connect(decline_proposal,
                                     all_models.Proposal,
                                     weak=False)
