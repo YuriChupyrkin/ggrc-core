4433f05fea4a589867d75f448550a6ae8c475f8f
YuriChupyrkin <yu4.1990@gmail.com>
2017-12-07 15:12:21 +0300

    Show custom attributes on proposal modal

--- src/ggrc/assets/javascripts/controllers/modals_controller.js
+++ src/ggrc/assets/javascripts/controllers/modals_controller.js
@@ -389,6 +389,7 @@ export default can.Control({
 
   draw: function (content, header, footer, customAttributes) {
     var modalTitle = this.options.modal_title;
+    var isProposal = this.options.isProposal;
     var isObjectModal = modalTitle && (modalTitle.indexOf('Edit') === 0 ||
       modalTitle.indexOf('New') === 0);
     var $form;
@@ -422,7 +423,7 @@ export default can.Control({
       this.options.$footer.html(footer);
     }
 
-    if (customAttributes != null && isObjectModal) {
+    if (customAttributes != null && (isObjectModal || isProposal)) {
       this.options.$content.append(customAttributes);
     }
     this.setup_wysihtml5();
