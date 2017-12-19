122d9388bfc1ffab39bd5b702098d74ae78a1502
YuriChupyrkin <yu4.1990@gmail.com>
2017-12-19 18:18:59 +0300

    Compare revisions in Revision History tab

--- src/ggrc/assets/javascripts/components/related-objects/revisions/related-revisions-item.js
+++ src/ggrc/assets/javascripts/components/related-objects/revisions/related-revisions-item.js
@@ -10,6 +10,7 @@ import '../../diff/instance-mapping-fields-diff';
 import '../../diff/instance-list-fields-diff';
 import '../../object-history/fields-diff-items';
 import '../../object-history/list-fields-diff-items';
+import '../../revision-history/restored-revision-comparer-config';
 import template from './templates/related-revisions-item.mustache';
 const tag = 'related-revisions-item';
 
@@ -18,6 +19,17 @@ export default can.Component.extend({
   template,
   viewModel: {
     define: {
+      instance: {
+        set(newValue, setValue) {
+          if (!newValue) {
+            return;
+          }
+
+          // revision-comparer expects view path
+          newValue.attr('view', this.getInstanceView(newValue));
+          setValue(newValue);
+        },
+      },
       revision: {
         set(newValue, setValue) {
           if (!newValue) {
@@ -37,10 +49,18 @@ export default can.Component.extend({
         },
       },
     },
-    instance: {},
     modifiedBy: {},
-    review() {
-      // TODO: review logic
+    lastRevision: {},
+    getInstanceView(instance) {
+      let view;
+      let typeInfo;
+      if (instance.view) {
+        return instance.view;
+      }
+
+      typeInfo = instance.class.table_plural + '/info';
+      view = GGRC.mustache_path + '/' + typeInfo + '.mustache';
+      return view;
     },
   },
 });
