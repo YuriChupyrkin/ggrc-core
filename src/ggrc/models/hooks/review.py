b457f9c0e32f2779fe326ef775bd7b60ec915dca
pod2metra <pod2metra@gmail.com>
2018-04-12 19:14:49 +0300

    tmp

---
+++ src/ggrc/models/hooks/review.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""Review object hooks."""
+
+import datetime
+
+from sqlalchemy import inspect
+
+from ggrc.services import signals
+from flask.ext.login import current_user
+
+from ggrc.models import all_models
+from ggrc.models import review
+from sqlalchemy import event
+
+
+def update_reviewable_status_on_review_update(mapper, connection, target):
+  if not target.review:
+    return
+  target.review.status = all_models.Review.STATES.UNREVIEWED
+  target.review.agenda = "{user_email} made change in {target_slug}".format(
+      user_email = current_user.display_name,
+      target_slug = target.slug,
+  )
+
+
+def init_hook():
+  """Init proposal signal handlers."""
+  for model in all_models.all_models:
+    if issubclass(model, review.Reviewable):
+      event.listen(model,
+                   "after_update",
+                   update_reviewable_status_on_review_update)
