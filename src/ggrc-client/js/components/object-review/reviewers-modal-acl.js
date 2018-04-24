0117a1877a901b93fc43ecc3613d6f7e7ca1c6bb
YuriChupyrkin <yu4.1990@gmail.com>
2018-04-24 12:00:17 +0300

    Add 'reviewers-modal-acl' component

---
+++ src/ggrc-client/js/components/object-review/reviewers-modal-acl.js
@@ -0,0 +1,15 @@
+/*
+ Copyright (C) 2018 Google Inc., authors, and contributors
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+import template from './templates/reviewers-modal-acl.mustache';
+const tag = 'reviewers-modal-acl';
+
+export default can.Component.extend({
+  tag,
+  template,
+  viewModel: {
+    accessControlList: [],
+  },
+});
--- src/ggrc-client/js/components/object-review/reviewers-modal.js
+++ src/ggrc-client/js/components/object-review/reviewers-modal.js
@@ -3,6 +3,8 @@
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
 
+import './reviewers-modal-acl';
+
 import template from './templates/reviewers-modal.mustache';
 const tag = 'reviewers-modal';
 
