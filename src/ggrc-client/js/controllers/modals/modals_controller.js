e17e34be04b244428f49410c3a93f623b1561418
YuriChupyrkin <yu4.1990@gmail.com>
2018-07-06 10:27:01 +0300

    Remove unneeded refresh

--- src/ggrc-client/js/controllers/modals/modals_controller.js
+++ src/ggrc-client/js/controllers/modals/modals_controller.js
@@ -1132,9 +1132,6 @@ export default can.Control({
         });
         instance.attr('custom_attribute_definitions', cad);
       }
-      instance.notifier.on_empty(() => {
-        instance.refresh();
-      });
       instance.dispatch(REFRESH_MAPPING);
     }
   },
