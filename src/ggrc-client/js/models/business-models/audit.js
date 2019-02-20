901b4e084e5a734ae7d9eeccaf7a7cc48cfc544f
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-02-20 11:15:33 +0300

    Add define validation to all Cacheable models

--- src/ggrc-client/js/models/business-models/audit.js
+++ src/ggrc-client/js/models/business-models/audit.js
@@ -111,20 +111,6 @@ export default Cacheable.extend({
     if (this._super) {
       this._super(...arguments);
     }
-    /* Can be replaced with "Required" */
-    this.validatePresenceOf('program');
-    this.validateNonBlank('title');
-
-    // TODO: use "validateIssueTracker" extension
-    this.validate(
-      'issue_tracker_component_id',
-      function () {
-        if (this.attr('issue_tracker.enabled') &&
-          !this.attr('issue_tracker.component_id')) {
-          return 'cannot be blank';
-        }
-      }
-    );
   },
   buildIssueTrackerConfig() {
     return {
@@ -137,6 +123,26 @@ export default Cacheable.extend({
     };
   },
 }, {
+  define: {
+    title: {
+      value: '',
+      validate: {
+        required: true,
+      },
+    },
+    program: {
+      value: null,
+      validate: {
+        required: true,
+      },
+    },
+    issue_tracker: {
+      value: {},
+      validate: {
+        validateIssueTracker: true,
+      },
+    },
+  },
   clone: function (options) {
     let cloneModel = new this.constructor({
       operation: 'clone',
