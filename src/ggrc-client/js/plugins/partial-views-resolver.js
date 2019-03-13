d6037bce781462d7ab328844693983f409383eab
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-03-13 14:33:56 +0300

    Add partial view resolver

---
+++ src/ggrc-client/js/plugins/partial-views-resolver.js
@@ -0,0 +1,79 @@
+/*
+    Copyright (C) 2019 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+import infoPaneUtility from '../templates/base_objects/info-pane-utility.stache';
+import peopleDropdowMenu from '../templates/people/dropdown_menu.stache';
+import snapshotsDropdowMenu from '../templates/snapshots/dropdown_menu.stache';
+import auditsDropdowMenu from '../templates/audits/dropdown_menu.stache';
+import baseDropdownMenu from '../templates/base_objects/dropdown_menu.stache';
+import editObjectLink from '../templates/base_objects/edit_object_link.stache';
+import baseDescription from '../templates/base_objects/description.stache';
+import baseTestPlan from '../templates/base_objects/test_plan.stache';
+import baseNotes from '../templates/base_objects/notes.stache';
+import baseContacts from '../templates/base_objects/contacts.stache';
+import baseRefUrl from '../templates/base_objects/reference-urls.stache';
+import caInfo from '../templates/custom_attributes/info.stache';
+import baseOpenClose from '../templates/base_objects/open_close.stache';
+import baseFormRestore from '../templates/base_objects/form_restore.stache';
+import modalAjaxTestPlan from '../templates/partials/modal-ajax-test-plan.stache';
+import modalRefUrls from '../templates/base_objects/modal-content-reference-urls.stache';
+
+const registerPartial = can.stache.registerPartial;
+
+const partialViews = [
+  {
+    id: '/static/templates/base_objects/info-pane-utility.stache',
+    template: infoPaneUtility,
+  }, {
+    id: '/static/templates/snapshots/dropdown_menu.stache',
+    template: snapshotsDropdowMenu,
+  }, {
+    id: '/static/templates/audits/dropdown_menu.stache',
+    template: auditsDropdowMenu,
+  }, {
+    id: '/static/templates/people/dropdown_menu.stache',
+    template: peopleDropdowMenu,
+  }, {
+    id: '/static/templates/base_objects/dropdown_menu.stache',
+    template: baseDropdownMenu,
+  }, {
+    id: '/static/templates/base_objects/edit_object_link.stache',
+    template: editObjectLink,
+  }, {
+    id: '/static/templates/base_objects/description.stache',
+    template: baseDescription,
+  }, {
+    id: '/static/templates/base_objects/test_plan.stache',
+    template: baseTestPlan,
+  }, {
+    id: '/static/templates/base_objects/notes.stache',
+    template: baseNotes,
+  }, {
+    id: '/static/templates/base_objects/contacts.stache',
+    template: baseContacts,
+  }, {
+    id: '/static/templates/base_objects/reference-urls.stache',
+    template: baseRefUrl,
+  }, {
+    id: '/static/templates/custom_attributes/info.stache',
+    template: caInfo,
+  }, {
+    id: '/static/templates/base_objects/open_close.stache',
+    template: baseOpenClose,
+  }, {
+    id: '/static/templates/base_objects/form_restore.stache',
+    template: baseFormRestore,
+  }, {
+    id: '/static/templates/partials/modal-ajax-test-plan.stache',
+    template: modalAjaxTestPlan,
+  }, {
+    id: '/static/templates/base_objects/modal-content-reference-urls.stache',
+    template: modalRefUrls,
+  },
+];
+
+_.forEach(partialViews, (view) => {
+  registerPartial(view.id, view.template);
+});
