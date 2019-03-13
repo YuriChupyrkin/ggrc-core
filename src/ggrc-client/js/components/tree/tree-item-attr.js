0a5d5ff12e3d2067b6f81bf9efafd6d62a46406d
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-03-13 17:05:37 +0300

    Use viewModel as can.Map instead of js Object

--- src/ggrc-client/js/components/tree/tree-item-attr.js
+++ src/ggrc-client/js/components/tree/tree-item-attr.js
@@ -35,7 +35,7 @@ export default can.Component.extend({
   tag: 'tree-item-attr',
   template: can.stache(template),
   leakScope: true,
-  viewModel: {
+  viewModel: can.Map.extend({
     instance: null,
     name: '',
     define: {
@@ -93,5 +93,5 @@ export default can.Component.extend({
     isMarkdown() {
       return !!this.attr('instance').constructor.isChangeableExternally;
     },
-  },
+  }),
 });
