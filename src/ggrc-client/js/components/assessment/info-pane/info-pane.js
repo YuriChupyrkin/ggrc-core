4302ac2c0cff0f499168dcaa8530e6660fbcfb0c
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-03-13 17:05:37 +0300

    Use viewModel as can.Map instead of js Object

--- src/ggrc-client/js/components/assessment/info-pane/info-pane.js
+++ src/ggrc-client/js/components/assessment/info-pane/info-pane.js
@@ -75,7 +75,7 @@ export default can.Component.extend({
   tag: 'assessment-info-pane',
   template: can.stache(template),
   leakScope: true,
-  viewModel: {
+  viewModel: can.Map.extend({
     define: {
       verifiers: {
         get: function () {
@@ -620,7 +620,7 @@ export default can.Component.extend({
       this.attr('previousStatus', undefined);
       this.attr('isUndoButtonVisible', false);
     },
-  },
+  }),
   init: function () {
     this.viewModel.initializeFormFields();
     this.viewModel.initGlobalAttributes();
