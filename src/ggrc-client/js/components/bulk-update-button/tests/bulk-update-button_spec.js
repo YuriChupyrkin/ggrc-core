6c0b8cdccd78ac10137b1c0b25ee7e31d4494b55
Roman Matusevich <roma.matusevich@gmail.com>
2019-04-12 19:13:58 +0300

    Use can-event.trigger instead of can.trigger

--- src/ggrc-client/js/components/bulk-update-button/tests/bulk-update-button_spec.js
+++ src/ggrc-client/js/components/bulk-update-button/tests/bulk-update-button_spec.js
@@ -3,6 +3,7 @@
   Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
+import canEvent from 'can-event';
 import updateService from '../../../plugins/utils/bulk-update-service';
 import {getComponentVM} from '../../../../js_specs/spec_helpers';
 import Component from '../bulk-update-button';
@@ -64,7 +65,7 @@ describe('bulk-update-button component', function () {
       resMessage = 'items updated';
       updateDfd = $.Deferred();
 
-      spyOn(can, 'trigger');
+      spyOn(canEvent, 'trigger');
       spyOn(NotifiersUtils, 'notifier');
       spyOn(updateService, 'update')
         .and.returnValue(updateDfd);
@@ -109,7 +110,7 @@ describe('bulk-update-button component', function () {
         updateDfd.resolve([]);
 
         expect(el.closest).not.toHaveBeenCalled();
-        expect(can.trigger)
+        expect(canEvent.trigger)
           .not.toHaveBeenCalled();
       });
 
